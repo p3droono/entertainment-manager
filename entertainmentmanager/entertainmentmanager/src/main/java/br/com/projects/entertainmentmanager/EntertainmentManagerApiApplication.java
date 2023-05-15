5630b4650a7d7cdba0acb308196c069553ffe2b9
Pedro Ono <pedro.ono@hotmail.com>
2023-05-15 12:04:07 -0300

    Add pom.xml

---
+++ entertainmentmanager/entertainmentmanager/src/main/java/br/com/projects/entertainmentmanager/EntertainmentManagerApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.projects.entertainmentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntertainmentManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntertainmentManagerApiApplication.class, args);
+	}
+
+}
